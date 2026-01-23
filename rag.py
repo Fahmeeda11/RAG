39c3f5c4dd92cac472c2692b73ca695e67b6daa8
Fahmeeda11 <fahmeedak7867@gmail.com>
2026-01-23 21:18:39 +0530

    created query file for only search related stuff from llm. loading of data complied in rag file.

--- rag.py
+++ rag.py
@@ -1,26 +1,18 @@
-#load the data
 #import necessary libraries and create env
-import argparse
 import getpass
 import os
-import openai
-from langchain_core.documents import Document
 from dotenv import load_dotenv
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_classic.chains.retrieval_qa.base import RetrievalQA
+
 #env variables
 load_dotenv()
 USER_AGENT= os.getenv('USER_AGENT')
 ROOT_PATH = os.getenv('SINGLE_FILE_PATH')
 OPENAI_API_KEY = getpass.getpass(os.getenv('OPENAI_API_KEY'))
 CHROMA_PATH = os.getenv('CHROMA_PATH')
-##to load data from web pages we can use WebBaseLoader. lanchain provides various document loaders to load data from different sources. 
-from langchain_community.document_loaders import WebBaseLoader
 
 # function to load pdf files from a folder
 def load_pdf(DATA_PATH):
@@ -67,43 +59,17 @@ def embedding_function():
     return embeddings
 
 
+#calling load_pdf function with variable.
 list_of_docs = load_pdf(ROOT_PATH)
 print(f"loaded {len(list_of_docs)} documents from {ROOT_PATH}.")
+
+#calling split_documents function with chunks variable
 chunks = split_documents(list_of_docs)
 
-PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
-
-{context}
-
----
-
-Answer the question based on the above context: {question}
-"""
-
-#adding parser
-parser = argparse.ArgumentParser()
-parser.add_argument("query_text", type=str, help="Query text to search in the documents")
-args = parser.parse_args()
-query_text = args.query_text
-
-#embeddings
+#embeddings 
 embeddings = embedding_function()
 
 #create vector store using Chroma
 db =Chroma(collection_name="documents", embedding_function=embeddings, persist_directory=CHROMA_PATH)
 db.add_documents(chunks)
-
-results= db.similarity_search_with_relevance_scores(query_text, k=1)
-
-context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-prompt = prompt_template.format(context=context_text, question=query_text)
-print(prompt)
-
-model = ChatOpenAI()
-response = model.ainvoke(prompt)
-print("Response:", response)
-#sources
-sources = ("\n".join([doc.metadata["source"] for doc, _ in results]))
-print("Sources:", sources)
+print("added documents to Chroma vector store:", db)
