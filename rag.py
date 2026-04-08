a1c237e5c531bdef1ba6a5d53a480a918112e8e3
Fahmeeda11 <fahmeedak7867@gmail.com>
2026-04-08 22:01:22 +0530

    implementing upload feature1

--- rag.py
+++ rag.py
@@ -7,82 +7,83 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 
-#env variables
-load_dotenv()
-USER_AGENT= os.getenv('USER_AGENT')
-ROOT_PATH = os.getenv('DATA_PATH')
-OPENAI_API_KEY = getpass.getpass(os.getenv('OPENAI_API_KEY'))
-CHROMA_PATH = os.getenv('CHROMA_PATH')
+def main():
+    #env variables
+    load_dotenv()
+    USER_AGENT= os.getenv('USER_AGENT')
+    ROOT_PATH = os.getenv('DATA_PATH')
+    OPENAI_API_KEY = getpass.getpass(os.getenv('OPENAI_API_KEY'))
+    CHROMA_PATH = os.getenv('CHROMA_PATH')
 
 
-# function to load pdf files from a folder
-def load_pdf(DATA_PATH):
-    print(f"loading pdf files from {DATA_PATH}...")
-    all_docs = [] #list to store all documents
-    for file_path in os.listdir(DATA_PATH): #in this folder go over each thing
-        if file_path.endswith(".pdf"): # if thing name ends in .pdf
-            file_path = os.path.join(DATA_PATH, file_path) #get full path of thing
-            
-            loaders = PyPDFLoader(file_path) #load the pdf
-            all_docs.append( loaders.load()) #append the loaded pdf to the list
-            
-        else:
-            #call the function recursively if there are subfolders
-            sub_dir = os.path.join(DATA_PATH, file_path)
-            if os.path.isdir(sub_dir):
-                all_docs.extend(load_pdf(sub_dir))
-    print(f"loaded {len(all_docs)} documents from {DATA_PATH}.")
-    return all_docs
+    # function to load pdf files from a folder
+    def load_pdf(DATA_PATH):
+        print(f"loading pdf files from {DATA_PATH}...")
+        all_docs = [] #list to store all documents
+        for file_path in os.listdir(DATA_PATH): #in this folder go over each thing
+            if file_path.endswith(".pdf"): # if thing name ends in .pdf
+                file_path = os.path.join(DATA_PATH, file_path) #get full path of thing
+                
+                loaders = PyPDFLoader(file_path) #load the pdf
+                all_docs.append( loaders.load()) #append the loaded pdf to the list
+                
+            else:
+                #call the function recursively if there are subfolders
+                sub_dir = os.path.join(DATA_PATH, file_path)
+                if os.path.isdir(sub_dir):
+                    all_docs.extend(load_pdf(sub_dir))
+        print(f"loaded {len(all_docs)} documents from {DATA_PATH}.")
+        return all_docs
 
-#function to split documents into chunks
-def split_documents(documents):
-    chunks = []
-    for doc in documents:
-        text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=80,
-        length_function=len,
-        is_separator_regex=False,
-    )
-        doc_chunks = text_splitter.split_documents(doc)
-        chunks.extend(doc_chunks)
-    print(f"split {len(documents)} documents into {len(chunks)} chunks.")
-    return chunks
-
-
-
-#function to create embeddings
-def embedding_function():
-    print("creating embeddings using OpenAI...")
-    #create embeddings using OpenAI
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-3-large"
-                )
-    print("created embeddings using OpenAI.")
-    return embeddings
+    #function to split documents into chunks
+    def split_documents(documents):
+        chunks = []
+        for doc in documents:
+            text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=800,
+            chunk_overlap=80,
+            length_function=len,
+            is_separator_regex=False,
+        )
+            doc_chunks = text_splitter.split_documents(doc)
+            chunks.extend(doc_chunks)
+        print(f"split {len(documents)} documents into {len(chunks)} chunks.")
+        return chunks
 
 
-#calling load_pdf function with variable.
-list_of_docs = load_pdf(ROOT_PATH)
-print(f"loaded {len(list_of_docs)} documents from {ROOT_PATH}.")
 
-#calling split_documents function with chunks variable
-chunks = split_documents(list_of_docs)
+    #function to create embeddings
+    def embedding_function():
+        print("creating embeddings using OpenAI...")
+        #create embeddings using OpenAI
+        embeddings = OpenAIEmbeddings(
+            model="text-embedding-3-large"
+                    )
+        print("created embeddings using OpenAI.")
+        return embeddings
 
-#batch size for chunks
 
-#embeddings 
-embeddings = embedding_function()
+    #calling load_pdf function with variable.
+    list_of_docs = load_pdf(ROOT_PATH)
+    print(f"loaded {len(list_of_docs)} documents from {ROOT_PATH}.")
 
-#create vector store using Chroma
-db =Chroma(collection_name="documents", embedding_function=embeddings, persist_directory=CHROMA_PATH)
-# db.add_documents(chunks)
-BATCH_SIZE = 1000
-for i in range(0, len(chunks), BATCH_SIZE):
-    batch_chunks = chunks[i:i + BATCH_SIZE]
-    print(f"processing batch {i//BATCH_SIZE + 1}: adding {len(batch_chunks)} chunks to vector store...")
-    db.add_documents(batch_chunks)
-    
-print("added documents to Chroma vector store:", db)
+    #calling split_documents function with chunks variable
+    chunks = split_documents(list_of_docs)
+
+    #batch size for chunks
+
+    #embeddings 
+    embeddings = embedding_function()
+
+    #create vector store using Chroma
+    db =Chroma(collection_name="documents", embedding_function=embeddings, persist_directory=CHROMA_PATH)
+    # db.add_documents(chunks)
+    BATCH_SIZE = 1000
+    for i in range(0, len(chunks), BATCH_SIZE):
+        batch_chunks = chunks[i:i + BATCH_SIZE]
+        print(f"processing batch {i//BATCH_SIZE + 1}: adding {len(batch_chunks)} chunks to vector store...")
+        db.add_documents(batch_chunks)
+        
+    print("added documents to Chroma vector store:", db)
 
 
--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import streamlit
 from dataclasses import dataclass
 from query import get_response #importing function from other py file
+from langchain_community.document_loaders import PyPDFLoader
 
 
 streamlit.title("RAG Chatbot with Streamlit and OpenAI") #title for the page
@@ -9,7 +10,8 @@ streamlit.title("RAG Chatbot with Streamlit and OpenAI") #title for the page
 client = OpenAI(api_key=streamlit.secrets[("OPENAI_API_KEY")])
 print("OpenAI API Key loaded successfully.") #testing prints
 
-
+if "docs_loaded" not in streamlit.session_state:
+    streamlit.session_state.docs_loaded = False
 
 if "openai_model" not in streamlit.session_state:
     streamlit.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -19,34 +21,77 @@ if "messages" not in streamlit.session_state:
     streamlit.session_state.messages = []
 print("Session state messages initialized.")
 
+streamlit.session_state.docs_loaded = True
+print("Documents loaded and processed:", streamlit.session_state.docs_loaded)
+
+
+#upload ui
+if not streamlit.session_state.docs_loaded:
+    uploaded_files = streamlit.file_uploader(label="Upload PDF files", accept_multiple_files=True, type=["pdf"])
+    if uploaded_files:
+        with streamlit.spinner("Processing..."):
+            file_path = "./doc_files/"
+            for file in uploaded_files:
+                with open(file_path + file.name, "wb") as f:
+                    f.write(file.getvalue())
+                loader = PyPDFLoader(file_path)
+                pages = loader.load_and_split()
+            
+
+#chat ui
 for message in streamlit.session_state.messages:
     with streamlit.chat_message(message["role"]):
         streamlit.markdown(message["content"])
 print("Displayed previous messages.")
-if prompt := streamlit.chat_input("What is up?", accept_file="directory", file_type=["pdf"]):
+
+
+if prompt := streamlit.chat_input("What is up?"):
     streamlit.session_state.messages.append({"role": "user", "content": prompt})
     with streamlit.chat_message("user"):
         streamlit.markdown(prompt)
-if prompt and prompt["files"]:
-    streamlit.image(prompt["files"][0])
-    # if prompt and prompt["files"]:
-    #     streamlit.image(prompt["files"][0])
-    print("User prompt added to session state messages.")
+        print("User prompt added to session state messages.")
     with streamlit.chat_message("assistant"):
         content,sources = get_response(prompt)
         streamlit.markdown(content)
+        print("Assistant response generated1.")
         with streamlit.expander("sources"):
             streamlit.text(sources)
-    with streamlit.chat_message("assistant"):
-        uploaded_files = prompt["files"]
-        streamlit.markdown(uploaded_files)
-        # with streamlit.chat_message("files"):
-        #     uploaded_files = prompt.files
-        #     if uploaded_files:
-        #         for file in uploaded_files:
-        #             streamlit.image(file)
-        
-        print("Assistant response generated.")
+            print("Assistant response generated.")
     streamlit.session_state.messages.append({"role": "assistant", "content": content})
     print("Assistant response added to session state messages.")
+
+
+
+# prompt = streamlit.chat_input(
+#     "Say something and/or attach an image",
+#     accept_file=True,
+#     file_type=["pdf"],
+# )
+# print("User input received:", prompt)
+# if prompt:
+    # streamlit.session_state.messages.append({"role": "user", "content": prompt})
+    # print("User prompt added to session state messages.")
+    # with streamlit.chat_message("user"):
+    #     streamlit.markdown(prompt)
+    #     print("User prompt displayed in chat.")
+    # with streamlit.pdf(prompt):
+    #     streamlit.markdown(prompt)
+    #     print("User PDF displayed in chat.")
+    
+     # print("User prompt added to session state messages.")
+    # with streamlit.chat_message("assistant"):
+    #     content,sources = get_response(prompt)
+    #     streamlit.markdown(content)
+    #     with streamlit.expander("sources"):
+    #         streamlit.text(sources)
+    # with streamlit.chat_message("assistant"):
+    #     uploaded_files = prompt["files"]
+    #     streamlit.markdown(uploaded_files)
+    #     with streamlit.chat_message("files"):
+    #         uploaded_files = prompt.files
+    #         if uploaded_files:
+    #             for file in uploaded_files:
+    #                 streamlit.image(file)
+        
+        
 
